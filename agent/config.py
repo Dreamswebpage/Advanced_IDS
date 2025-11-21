d12dfe3e5c096fdcbd220c47a1651499c46f12f8
abhi sharma <abhisharma968089@gmail.com>
2025-11-22 00:27:30 +0530

    Update config.py

--- agent/config.py
+++ agent/config.py
@@ -1,9 +1,4 @@
-BACKEND_URL = "https://abhiids.onrender.com/api/events"   # Render backend URL
-
-# Windows network interface name
-# If you're using mobile hotspot, interface is usually "Wi-Fi"
+BACKEND_URL = "https://abhiids.onrender.com/api/events"
 INTERFACE = "Wi-Fi"
-
-# Packet batching
-BATCH_SIZE = 10       # Number of packets per batch
-SEND_INTERVAL = 5     # Send interval in seconds
+BATCH_SIZE = 10
+SEND_INTERVAL = 5
